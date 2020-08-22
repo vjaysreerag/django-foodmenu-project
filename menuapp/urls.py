e34bbc1710c2b2d5661e02748e75ac7a5c7ea7cf
root <sreerag.vj90@gmail.com>
2020-08-21 22:50:30 -0400

    Second commit of the Project

--- menuapp/urls.py
+++ menuapp/urls.py
@@ -1,15 +1,10 @@
 from django.conf.urls import url
-from django.urls import path
+from django.urls import path, include
 from . import views
-
-app_name='menuapp'
 
 urlpatterns=[
   path('',views.home,name='home'),
-  path('',views.indian,name='indian'),
-  path('',views.chinese,name='chinese'),
-  path('',views.vegindian,name='vegindian')
-
-
-
+  path('indian/',views.indian,name='indian'),
+  path('chinese/',views.chinese,name='chinese'),
+  path('indian/vegindian/',views.vegindian,name='vegindian'),
 ]  
